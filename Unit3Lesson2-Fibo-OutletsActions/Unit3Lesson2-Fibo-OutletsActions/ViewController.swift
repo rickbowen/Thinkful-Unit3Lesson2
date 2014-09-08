e21c3982c4ac804cba3bd78ebe82901bc53be2df
Rick Bowen <rick@bowenux.com>
2014-09-07 21:04:20 -0400

    Add stepper UI element
    Stepper can increment fibo as well as slider. Stepper and slider stay in sync also

--- Unit3Lesson2-Fibo-OutletsActions/Unit3Lesson2-Fibo-OutletsActions/ViewController.swift
+++ Unit3Lesson2-Fibo-OutletsActions/Unit3Lesson2-Fibo-OutletsActions/ViewController.swift
@@ -13,21 +13,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var theSlider: UISlider!
     @IBOutlet weak var selectedValueLabel: UILabel!
     @IBOutlet weak var outputTextView: UITextView!
+    @IBOutlet weak var theStepper: UIStepper!
+    
     
     //An instance of the model
     var fibo = FibonacciModel()
+   
+    
+    @IBAction func stepperDidStep(sender: UIStepper) {
+        
+        updateFibo(Int(theStepper.value))
+        
+    }
     
    
     @IBAction func sliderValueDidChange(sender: UISlider) {
+        
+        updateFibo(Int(theSlider.value))
+        
+    }
     
+   
+    func updateFibo (var num: Int){
+        
         var returnedArray: [Int] = []
         var formattedOutput: String = ""
         
         //Display the updated slider value
-        self.selectedValueLabel.text = String(Int(theSlider.value))
+        self.selectedValueLabel.text = String(num)
+        
+        //Sync inputs
+        syncInputs(num)
         
         //Calculate the Fibonacci elements based on the new slider value
-        returnedArray = self.fibo.calculateFibonacciNumbers(minimum2: Int(theSlider.value))
+        returnedArray = self.fibo.calculateFibonacciNumbers(minimum2: num)
         
         //Put the elements into a nicely formatted array
         for number in returnedArray{
@@ -38,10 +57,13 @@ class ViewController: UIViewController {
         
         //Update the textfield with the formatted array
         self.outputTextView.text = formattedOutput
+        
     }
     
-   
-    
+    func syncInputs(var num: Int){
+        theSlider.value = Float(num)
+        theStepper.value = Double(num)
+    }
  
     
     
